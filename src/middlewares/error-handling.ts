d3d536b6e77d4d694109fa8961a7e01c20b7c26e
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-06-24 14:41:43 -0300

    chore: added zod and configured exception handling

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,6 +1,6 @@
 import { AppError } from "@/utils/AppError";
 import { Request, Response, NextFunction } from "express";
-
+import { ZodError } from "zod";
 export function errorHandling(
   error: any,
   request: Request,
@@ -9,6 +9,13 @@ export function errorHandling(
 ) {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return response.status(400).json({
+      message: "validation Error",
+      issues: error.format(),
+    });
   }
   return response.status(500).json({ message: error.message });
 }
