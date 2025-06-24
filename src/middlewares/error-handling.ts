297d53e26d08bedb0d24ed3389669e63013c3760
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-06-24 14:29:54 -0300

    feat: created error handling middleware

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,14 @@
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express";
+
+export function errorHandling(
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
+  }
+  return response.status(500).json({ message: error.message });
+}
