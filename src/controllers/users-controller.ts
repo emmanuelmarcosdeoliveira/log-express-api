2d2122947818b721d9316bef1197c4a230bf2291
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-07-04 12:51:40 -0300

    feat:  created tests to verify user creation

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -26,7 +26,7 @@ class UserController {
       },
     });
     const { password: _, ...userWithoutPassword } = user;
-    return response.json(userWithoutPassword);
+    return response.status(201).json(userWithoutPassword);
   }
 }
 export { UserController };
